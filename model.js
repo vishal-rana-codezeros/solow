3e160edb633fd99b1ede9b94ac57eea3d87ae946
vishal rana <vishal.rana@codezeros.com>
2018-08-24 15:45:30 +0530

    model change

--- model.js
+++ model.js
@@ -3,19 +3,30 @@ const schema  = mongoose.Schema;
 
 const userSchema = new schema({
 fullname:String,
-username:String,
 password:String,
 email_id:String,
-age:Number,
-profession_id:String,
-city_id:String,
-religion:String,
+country:String,
 gender:{type:String,enum:['Male','Female'],default:'Male'},
 createdAt:{type:Date,default:Date.now},
 type:{type:String,enum:['normal','facebook'],default:'normal'},
 createdAt:{type:Date,default:Date.now},
 fb_id:String,
-status:{type:String,default:'ACTIVE'}
+status:{type:String,default:'ACTIVE'},
+notify_flag:[{
+  order_number:String,
+  price_decrease:{type:Number,default:0},
+  price_increase:{type:Number,default:0},
+  price_dec_by:{
+    status:{type:Number,default:0},
+    price:Number
+  },
+  rooms_limited:{type:Number,default:0},
+  seats_limited:{type:Number,default:0},
+  buy_auto_price:{
+    status:{type:Number,default:0},
+    price:Number
+  }
+}]
 }) ;
 
 
